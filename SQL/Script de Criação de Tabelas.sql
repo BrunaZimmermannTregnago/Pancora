f7b6ceb35c6f5e718895cbb668aa11009ffc45fc
Ana-Epping <luizaana16390@gmail.com>
2021-06-18 19:58:41 -0300

    Adição de coluna "pacsex" na tabela "Paciente" no modelo relacional e no script de criação.

--- SQL/Script de Criação de Tabelas.sql
+++ SQL/Script de Criação de Tabelas.sql
@@ -200,3 +200,7 @@ alter table tipo_usuario drop column empcnpj;
 
 -- criando coluna fk tipusuid na tabela paciente
 alter table paciente add column tipusuid int4 not null constraint pac_tipusuid_fk references Tipo_Usuario (tipusuid);
+
+-- adicionando coluna pacsex na tabela paciente
+alter table paciente add column pacsex char(1) not null constraint pac_pacsex_ck check (pacsex in ('F','M','N','O'));
+comment on column paciente.pacsex is 'Sexo do paciente (F - Feminino, M - Masculino, N - Não-binário, O - Outro).';
