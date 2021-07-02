8afbd9a3e0f510923b362a060ce6213a99fbdc34
Ana-Epping <luizaana16390@gmail.com>
2021-07-02 16:56:39 -0300

    Alterações de index e adição de lista de requisitos.

--- SQL/script-index.sql
+++ SQL/script-index.sql
@@ -1,19 +1,46 @@
 -- INDEX
 
-create index paciente_info_index on
-paciente (paccpf, pacnom, pacsex, pacdatnas, pacpes, pacalt);
+create index paciente_nome 
+on paciente(pacnom);
 
-create index paciente_contato_index on
-paciente (pactel, pacema, paccep, cidid, baiid, pacendrua, pacendnum);
+create index paciente_usuario
+on paciente(tipusuid);
 
-create index empresa_index on
-empresa (empnomfan, empcnpj, emprazsoc, emptel, empema);
+create index paciente_cidade
+on paciente(cidid);
 
-create index sintoma_index on
-sintoma (sinid, sindes);
+create index empresa_nome
+on empresa(empnomfan);
 
-create index situacao_index on 
-situacao (sitid, sitpac);
+create index empresa_cidade
+on empresa(cidid);
 
-create index comorbidade_index on 
-comorbidade (comid, comdes);
+create index emp_paciente_empresa
+on empresa_paciente(empcnpj);
+
+create index emp_paciente_paciente
+on empresa_paciente(paccpf);
+
+create index sintoma_nome
+on sintoma(sindes);
+
+create index sin_paciente_paciente
+on sintoma_paciente(paccpf);
+
+create index situacao_nome
+on situacao(sitpac);
+
+create index sit_paciente_paciente
+on situacao_paciente(paccpf);
+
+create index comorbidade_nome
+on comorbidade(comdes);
+
+create index vac_paciente_paciente
+on vacina_paciente(paccpf);
+
+create index vac_paciente_vacina
+on vacina_paciente(vacid);
+
+create index cidade_uf
+on cidade(ufid);
