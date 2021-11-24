7eb8c3221511b68a724365d2bdf7da3e65c27698
Ana-Epping <luizaana16390@gmail.com>
2021-11-23 22:05:28 -0300

    Backend do sistema de login e autenticação por email.

--- API/src/main/java/br/edu/unoesc/springboot/pancora/repository/BairroRepository.java
+++ API/src/main/java/br/edu/unoesc/springboot/pancora/repository/BairroRepository.java
@@ -2,7 +2,6 @@ package br.edu.unoesc.springboot.pancora.repository;
 
 import br.edu.unoesc.springboot.pancora.entities.Bairro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BairroRepository extends JpaRepository<Bairro, Integer> {
 }
--- API/src/main/java/br/edu/unoesc/springboot/pancora/repository/PacienteRepository.java
+++ API/src/main/java/br/edu/unoesc/springboot/pancora/repository/PacienteRepository.java
@@ -3,6 +3,7 @@ package br.edu.unoesc.springboot.pancora.repository;
 import br.edu.unoesc.springboot.pancora.entities.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import java.math.BigDecimal;
 import java.util.List;
 
